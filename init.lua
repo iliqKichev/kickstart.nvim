f352318bf117094b9eb0ff55a6446d499ee7d486
Ilia <kichev.iliq@gmail.com>
2025-06-10 15:45:36 +0200

    Texlab integration for LaTeX

--- init.lua
+++ init.lua
@@ -554,7 +554,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`tsserver`) will work just fine
         -- tsserver = {},
         --
-
+        texlab = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
